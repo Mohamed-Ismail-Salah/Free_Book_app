ff98b372acbb9b2f8fc10a985c89e0ccfc625e21
Mohamed-Ismail-Salah <mi1230955@gmail.com>
2023-05-02 19:08:16 +0300

    Search ui

--- lib/Features/search/presentation/view/widget/SearchBody.dart
+++ lib/Features/search/presentation/view/widget/SearchBody.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/styles.dart';
 import 'Custom_Search_TextField.dart';
+import 'custom_BestResltBookListView.dart';
 
 class SearchBody extends StatelessWidget{
   const SearchBody({super.key});
@@ -9,10 +11,19 @@ class SearchBody extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return  SafeArea(
-      child: Column(
-        children:const [
-          CustomSearchTextField()
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+            const CustomSearchTextField(),
+             Padding(
+               padding: const EdgeInsets.symmetric(vertical: 16),
+               child: Text( "Search Result",style: Styles.textStyle20.copyWith(fontWeight: FontWeight.w700),),
+             ),
+             const Expanded(child: SearchResultBookListView())
+          ],
+        ),
       ),
     );
   }
